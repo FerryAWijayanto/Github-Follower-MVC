55345b4e6ab496c8c0f569a5544b87f473b95369
ferry adi wijayanto <justinblackvfx@gmail.com>
2021-05-02 05:58:34 +0700

    Initial Commit

---
+++ Github Follower MVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Follower MVC
+//
+//  Created by Ferry Adi Wijayanto on 02/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
